4d5f4d3cf70d0693666b1d28594be7273d05f3d0
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2019-10-14 22:27:17 -0400

    Add project files.

---
+++ Iowa06/grid1.bat
@@ -0,0 +1,6 @@
+D:\Maizsim07\CreateSoils\CreateSoilFiles.exe "D:\MAIZSIM07\AgMipEt\Iowa06\Iowa06.lyr" /GN Iowa06 /SN Harps
+del output
+del element_elm
+del grid_bnd
+del datagen2.dat
+Dir  *.*  >dir.txt
---
+++ Soil Source/Crop.for
@@ -0,0 +1,16 @@
+      Subroutine Crop()
+      Include 'Public.ins'
+      Include 'puplant.ins'
+        
+c initialize here      
+      If (lInput.eq.1) then
+      
+        LAI=3.4
+        cec=0.65
+        nShoot=1
+      
+      End If
+      
+      Return
+      End
+      
