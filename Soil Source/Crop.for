92afe5399b19f471a3b1c227efe847e1aed0f372
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2019-10-24 16:49:17 -0400

    et now works

--- Soil Source/Crop.for
+++ Soil Source/Crop.for
@@ -22,6 +22,7 @@ c        initialize roots
         dify = 55.0
         M=50.0
         rootTime=100  ! assume a fully developed root system
+        COVER=1.0 - exp (-CEC*LAI)
         Do i= 1, NumNP
          
          Yr(i)=  y(1)-y(i)  ! 0 is relative to bottom in grid but must be relative to the surface for root calcs
@@ -30,7 +31,10 @@ c        initialize roots
          RTWT(i) = root1*Exp(-root2)
        End Do
       End If
-      
+        ET_demand=2.5 !just a stub for now, need to get this from weather.
+c     TPot is g plant-1 day-1, ET_demand should be g m-2 day
+c     1/poprow*100/Rowsp converts to g m-2 day-1
+      ET_Demand=TPot/poprow*100/RowSp
       Return
 10    stop "error in crop file"      
       End
